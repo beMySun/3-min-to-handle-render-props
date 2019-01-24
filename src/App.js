68242b1614b4a690e6ba76b3fb0f140a14bb2900
KK <yongkang.yan@shopee.com>
2019-01-24 11:17:10 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,185 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import './index.css';
+
+const clip = 'polygon(0 5%, 5% 0, 95% 0, 100% 5%, 100% 95%, 95% 100%, 5% 100%, 0 95%)';
+const Strap = styled.div`
+  clip-path: ${clip};
+  height: 300px;
+  overflow: hidden;
+  position: relative;
+  width: 100px;
+`
+const Hand = styled.div`
+  width: ${p => (p.type === 'seconds' ? 2 : 5)}px;
+  height: ${p => (p.type ? 40 : 20)}px;
+  background: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform-origin: bottom;
+  transform: translate(-50%, -100%) rotate(${p => p.value}deg);
+`
+
+const Bezel = styled.div`
+  background: silver;
+  height: 60%;
+  left: 50%;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  clip-path: ${clip};
+  width: 102%;
+`
+
+const Screen = styled.div`
+  height: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  overflow: hidden;
+  transform: translate(-50%, -50%);
+  width: 90%;
+  background: #000;
+  color: #fff;
+`
+
+const Face = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const Value = styled.div`
+  font-size: ${p => (p.small ? 2 : 3)}rem;
+  line-height: ${p => (p.small ? 2 : 3)}rem;
+  text-transform: uppercase;
+  text-align: center;
+  opacity: ${p => (p.opaque ? 0.5 : 1)};
+`
+
+const Day = styled.div`
+  text-transform: uppercase;
+  text-align: center;
+`
+
+const watchfacePropTypes = {
+  date: PropTypes.object,
+}
+
+const DefaultFace = ({ date }) => (
+  <>
+    <Value>{date.format('HH')}</Value>
+    <Value>{date.format('mm')}</Value>
+  </>
+)
+
+DefaultFace.propTypes = watchfacePropTypes;
+
+const DateFace = ({ date }) => {
+  const hours = date.format('HH')
+  const minutes = date.format('mm')
+  const monthYear = date.format('MMM DD')
+  return (
+    <>
+      <Value>{hours}</Value>
+      <Day>{monthYear}</Day>
+      <Value>{minutes}</Value>
+    </>
+  )
+}
+DateFace.propTypes = watchfacePropTypes;
+
+const SecondsFace = ({ date }) => {
+  const hours = date.format('HH')
+  const minutes = date.format('mm')
+  const seconds = date.format('ss')
+  return (
+    <>
+      <Value>{hours}</Value>
+      <Value>{minutes}</Value>
+      <Value>{seconds}</Value>
+    </>
+  )
+}
+SecondsFace.propTypes = watchfacePropTypes;
+
+const AnalogFace = ({ date }) => {
+  const seconds = (360 / 60) * date.seconds()
+  const minutes = (360 / 60) * date.minutes()
+  const hours = (360 / 12) * date.format('h')
+  return (
+    <>
+      <Hand type="seconds" value={seconds} />
+      <Hand type="minutes" value={minutes} />
+      <Hand value={hours} />
+    </>
+  )
+}
+AnalogFace.propTypes = watchfacePropTypes;
+
+
+const DayFace = ({ date }) => {
+  const hours = date.format('H')
+  const minutes = date.format('mm')
+  const dayN = date.format('dd')
+  const day = date.format('DD')
+  return (
+    <>
+      <Value small>{hours}</Value>
+      <Value small>{minutes}</Value>
+      <Value small opaque>
+        {dayN}
+      </Value>
+      <Value small opaque>
+        {day}
+      </Value>
+    </>
+  )
+}
+DayFace.propTypes = watchfacePropTypes
+
+class Watch extends Component {
+  state = {
+    date: moment(),
+  }
+  static propTypes = {
+    face: PropTypes.func,
+  }
+  static defaultProps = {
+    face: date => <DefaultFace date={date} />,
+  }
+  componentDidMount = () => (this.TICK = setInterval(this.update, 1000))
+  componentWillUnmount = () => clearInterval(this.TICK)
+  update = () => this.setState({ date: moment() })
+  render = () => (
+    <Strap>
+      <Bezel>
+        <Screen>
+          <Face>{this.props.face(this.state.date)}</Face>
+        </Screen>
+      </Bezel>
+    </Strap>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app">
+        <Watch />
+        <Watch face={date => <DayFace date={date} />} />
+        <Watch face={date => <AnalogFace date={date} />} />
+        <Watch face={date => <DateFace date={date} />} />
+        <Watch face={date => <SecondsFace date={date} />} />
+      </div>
+    );
+  }
+}
+
+export default App;
